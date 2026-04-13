d4cb84965c5a49e3809865bf5a5adee182bb995f
yuhang0824 <hangcy0824@gmail.com>
2026-04-14 00:23:07 +0800

    i think this is final version :)

--- src/Booking/Method.java
+++ src/Booking/Method.java
@@ -47,39 +47,27 @@ public class Method {
     }
     
     public static String generateBookingId() {
-        /// auto create booking id
-        
+        /// auto create Booking ID
         try {
-            ArrayList<String> booking = File.getData();
+            ArrayList<Booking> bookings = File.getData();
 
-            // if only have header
-            if (booking.size() <= 1) {
+            // if empty
+            if (bookings.isEmpty()) {
                 return "B0001";
             }
 
-            String lastLine = "";
+            // use last booking
+            int max = 0;
 
-            // Find the last valid row of data from the end to the beginning (safer).
-            for (int i = booking.size() - 1; i >= 1; i--) { // Start from end, skip the header.
-                if (!booking.get(i).trim().isEmpty()) {
-                    lastLine = booking.get(i);
-                    break;
+            for (Booking b : bookings) {
+                int num = Integer.parseInt(b.getBookingId().substring(1));
+                if (num > max) {
+                    max = num;
                 }
             }
+
+            return String.format("B%04d", max + 1);
             
-            // if is empty
-            if (lastLine.equals("")) {
-                return "B0001";
-            }
-
-            String[] data = lastLine.split(",");
-            String lastId = data[0];
-
-            int num = Integer.parseInt(lastId.substring(1));
-            num++;
-
-            return String.format("B%04d", num);
-
         } catch (Exception e) {
             e.printStackTrace();
             return "B0001";
@@ -103,32 +91,30 @@ public class Method {
         }
     }
     
-    public static int timeSelect(int timeChoice){
-        /// timeChoice 1 2 3 4 to correct time
+    public static String timeSelect(String timeChoice) {
+        /// time Choice 1 2 3 4 to String training
         
         switch (timeChoice) {
-            case 1: return 10;
-            case 2: return 13;
-            case 3: return 16;
-            case 4: return 19;
-            default: return 0;
+            case "1": return "10";
+            case "2": return "13";
+            case "3": return "16";
+            case "4": return "19";
+            default: return "0";
         }
     }
-    
+
     public static String formatTime(String time) {
-        int t = Integer.parseInt(time);
-
-        switch (t) {
-            case 10: return "10AM ~ 12PM";
-            case 13: return "1PM ~ 3PM";
-            case 16: return "4PM ~ 6PM";
-            case 19: return "7PM ~ 9PM";
-            default: return time; // fallback
+        /// display the actualy time
+        
+        switch (time) {
+            case "10": return "10AM ~ 12PM";
+            case "13": return "1PM ~ 3PM";
+            case "16": return "4PM ~ 6PM";
+            case "19": return "7PM ~ 9PM";
+            default: return time;
         }
     }
 
-
-    
     public static void bookingTime() {
         /// just display time that can be choose
         
